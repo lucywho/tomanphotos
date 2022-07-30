1f449165013907d27cf020c093a191fabf4c894b
Lucy Toman <lucy@toman.me.uk>
2022-07-30 17:31:02 +0200

    starts adding main photo route

--- components/Photo.js
+++ components/Photo.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Image from "next/image"
 
 export default function Photo({ photo }) {
@@ -5,27 +6,32 @@ export default function Photo({ photo }) {
 
     return (
         <div className="photobox">
-            {photo.title ? (
-                <p className="title">{photo.title}</p>
-            ) : (
-                <p className="title holding">no title</p>
-            )}
+            <Link href={`/photo/${photo.id}`}>
+                <>
+                    <p>{photo.id}</p>
+                    {photo.title ? (
+                        <p className="title">{photo.title}</p>
+                    ) : (
+                        <p className="title holding">no title</p>
+                    )}
 
-            {photo.url && (
-                <Image
-                    className="thumbnail"
-                    alt="thumbnail"
-                    src={link}
-                    height="200"
-                    width="200"
-                />
-            )}
+                    {photo.url && (
+                        <Image
+                            className="thumbnail"
+                            alt="thumbnail"
+                            src={link}
+                            height="200"
+                            width="200"
+                        />
+                    )}
 
-            {photo.info ? (
-                <p className="info">{photo.info}</p>
-            ) : (
-                <p className="info holding">no information</p>
-            )}
+                    {photo.info ? (
+                        <p className="info">{photo.info}</p>
+                    ) : (
+                        <p className="info holding">no information</p>
+                    )}
+                </>
+            </Link>
         </div>
     )
 }
