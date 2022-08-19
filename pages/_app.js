4ee34c1b4c96de26b55f397b43c5b68ab8aa2552
Lucy Toman <lucy@toman.me.uk>
2022-08-19 17:36:44 +0200

    adds next-auth and login

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css"
 import Header from "components/Header"
+import { SessionProvider } from "next-auth/react"
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Header />
-            <Component {...pageProps} />
+            <SessionProvider session={pageProps.session}>
+                <Header />
+                <Component {...pageProps} />
+            </SessionProvider>
         </>
     )
 }
