d0bd3d8125ae714e73f1c187b9c08279962e6082
Lucy Toman <lucy@toman.me.uk>
2022-08-25 11:24:14 +0200

    adds next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,13 +3,29 @@ import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "lib/prisma"
 
+let data
+
+if (process.env.NODE_ENV === "development") {
+    data = {
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+    }
+} else {
+    data = {
+        server: {
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD,
+            },
+        },
+        from: process.env.EMAIL_FROM,
+    }
+}
+
 export default NextAuth({
-    providers: [
-        EmailProvider({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
-        }),
-    ],
+    providers: [EmailProvider(data)],
 
     database: process.env.DATABASE_URL,
     secret: process.env.SECRET,
@@ -25,6 +41,7 @@ export default NextAuth({
     callbacks: {
         session: async ({ session, user }) => {
             session.user.id = user.id
+            session.user.isAdmin = user.isAdmin
             return Promise.resolve(session)
         },
     },
