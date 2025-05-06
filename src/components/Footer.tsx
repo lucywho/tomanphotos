6f3553a699d9e439a622f7cb2ab346bfbcc679b1
Lucy Toman <lucy.toman@thinslices.com>
2025-05-06 12:45:44 +0200

    Fixes position of footer and buttons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,24 +19,38 @@ export function Footer({
     if (typeof photoCode === "number") {
         return (
             <div className="footer">
-                <BackOne photoCode={photoCode} />
-                <ForwardOne photoCode={photoCode} />
+                <div className="footer-section-left">
+                    <BackOne photoCode={photoCode} />
+                </div>
+                <div className="footer-section-center" />
+                <div className="footer-section-right">
+                    <ForwardOne photoCode={photoCode} />
+                </div>
             </div>
         )
     }
 
     return (
         <div className="footer">
-            {onLoadLess && (
-                <button className="load-less" onClick={onLoadLess}>
-                    <ChevronsLeft color="var(--main-bg-color)" />
-                </button>
-            )}
-            {onLoadMore && (
-                <button className="load-more" onClick={onLoadMore}>
-                    <ChevronsRight color="var(--main-bg-color)" />
-                </button>
-            )}
+            <div className="footer-section-left">
+                {onLoadLess ? (
+                    <button className="load-less" onClick={onLoadLess}>
+                        <ChevronsLeft color="var(--main-bg-color)" />
+                    </button>
+                ) : (
+                    <div className="footer-spacer" />
+                )}
+            </div>
+            <div className="footer-section-center" />
+            <div className="footer-section-right">
+                {onLoadMore ? (
+                    <button className="load-more" onClick={onLoadMore}>
+                        <ChevronsRight color="var(--main-bg-color)" />
+                    </button>
+                ) : (
+                    <div className="footer-spacer" />
+                )}
+            </div>
         </div>
     )
 }
